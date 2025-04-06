e3b8a548a3eb0ddb588608f6541eb44f30b88f91
Jienshakh <dodinaev@nsk.beeline.ru>
2025-04-06 00:26:24 -0400

    added DISTRIB_CODENAME info

--- test.sh
+++ test.sh
@@ -9,3 +9,6 @@ cat /etc/*rel* | grep "DISTRIB_ID" | cut -d "=" -f2
 
 echo "DISTRIB_RELEASE:"
 cat /etc/*rel* | grep "DISTRIB_RELEASE" | cut -d "=" -f2
+
+echo "DISTRIB_CODENAME:"
+cat /etc/*rel* | grep "DISTRIB_CODENAME" | cut -d "=" -f2
